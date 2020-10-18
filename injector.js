76897274fdbcd210458398872d3f4080ccacf432
Josh <joshuaagkelley@gmail.com>
2020-10-18 17:14:54 +0100

    Added summary & title formatting.

--- injector.js
+++ injector.js
@@ -1,6 +1,8 @@
 var xhr = XMLHttpRequest.prototype;
 var send = xhr.send;
 
+var isStudent = true;
+
 // Making changes to XMLHttpRequest send function
 xhr.send = function(postData) {
     // Attach event listener to response
@@ -31,14 +33,45 @@ function parseJSON(arrayOfAllEvents) {
     var dtend = workingSingle.EndTime;
     dtend = dateTo8601(dtend)
     
-    var summary = null;
+    var summary = workingSingle.ModuleName;
+    summary = titleTrim(summary);
+    summary = titleCase(summary);
+    
     var location = null;
     var description = null;
     
     
     var returnBeans = buildIcs(uid, dtstamp, dtstart, dtend, summary, location, description);
+
     
-    console.log(returnBeans);
+    console.log(returnBeans)
+}
+
+
+function titleTrim(title) {
+    // Remove parenthesis from string
+    if (title.includes("(")) {
+        // Check parenthesis isn't first in string, and check student timetable.
+        if (title.indexOf("(") != 0 && isStudent) {
+            title = title.substring(0, title.indexOf("(") - 1);
+        }
+    }
+    // Lower-Case string
+    title = title.toLowerCase();
+    
+    if (title.includes(", ")) {
+        title = title.split(", ");
+        
+        for (var i = title.length - 1; i > 0; i--) {
+            if (title[i] == title[0]) {
+                title = title.pop();
+            }
+        }
+    }
+    
+    
+    
+    return title;
 }
 
 function dateTo8601(dateStr) {
@@ -112,4 +145,65 @@ function uniqueStr() {
         returnStr += randomList[randNum];
     }
     return returnStr;
+}
+
+// Title-Case strings
+function titleCase(inputString) {
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var lowerWords = ["a", "the", "in", "at", "and", "with", "to", "or", "of", "if", "it", "is", "but"];
+    var upperWords = ["pg", "ug", "l1", "l2", "l3", "l4", "l5", "l6", "l7", "l8", "bu", "bucss"];
+    var stringSplit = inputString.split(" ");
+    var endText;
+    
+    for (var i = 0; i < stringSplit.length; i++) {
+        // For each word, grab two strings, first character and the rest of the string
+        var firstChar = stringSplit[i].substring(0, 1);
+        var restChars = stringSplit[i].substring(1, stringSplit[i].length);
+        
+        // Capitalise words in upperWords
+        for (var j = 0; j < upperWords.length; j++) {
+            var proceed = true;
+            // Temporarily remove currently-checking upperWord from string
+            var cutString = stringSplit[i].replace(upperWords[j], "");
+            
+            // Check if new string still contains any A-Z letters
+            for (var k = 0; k < alphabet.length; k++) {
+                if (cutString.includes(alphabet[k])) {
+                    proceed = false;
+                }
+            }
+            
+            // Proceed if no other A-Z letters, and upperWord is in string
+            if (stringSplit[i].includes(upperWords[j]) && proceed) {
+                firstChar = firstChar.toUpperCase();
+                restChars = restChars.toUpperCase();
+            }
+        }
+        
+        if (i == 0 || !lowerWords.includes(stringSplit[i])) {
+            // Upper-Case first char if first word or not in lowerWords
+            firstChar = firstChar.toUpperCase();
+        }
+        
+        // Check if name contains hyphen
+        if (restChars.includes("-")) {
+            // Split name into three around hyphen
+            var leftStr = restChars.substring(0, restChars.indexOf("-")+1)
+            var midStr = restChars.substring(restChars.indexOf("-")+1, restChars.indexOf("-")+2);
+            var rightStr = restChars.substring(restChars.indexOf("-")+2, restChars.length);
+            // Capitalise letter after hyphen
+            restChars = leftStr + midStr.toUpperCase() + rightStr;
+        }
+        
+        // Merge first character and other characters
+        stringSplit[i] = firstChar + restChars;
+        
+        // Merge all words back into one string
+        if (endText == null) {
+            endText = firstChar + restChars;
+        } else {
+            endText = endText + " " + firstChar + restChars;
+        }
+    }
+    return (endText);
 }
