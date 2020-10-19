7b01c6ab6efc3117ca10dd6273bd8b12d604aa06
Josh <joshuaagkelley@gmail.com>
2020-10-19 16:52:13 +0100

    Removed unused files.

--- popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("btnDownload").onclick = function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {msg: "download"});
-    });
-}
