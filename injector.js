74b6cec8b50788270a2193bd38e4fad073c15236
Josh <joshuaagkelley@gmail.com>
2020-10-19 17:17:20 +0100

    Removed more unused code.

--- injector.js
+++ injector.js
@@ -14,15 +14,3 @@ imageElement.setAttribute("style", "display: none;");
 // Append script and image to DOM.
 (document.head || document.documentElement).appendChild(script);
 (document.head || document.documentElement).appendChild(imageElement);
-
-// TODO: Reimplement legacy download method.
-
-// Listen for keypress
-//document.addEventListener('keydown', logKey);
-// Run code on keypress
-//function logKey(e) {
-//    // Check if "S" key pressed (S = 83 in ASCII)
-//    if (e.keyCode == 83) {
-//        main();
-//    }
-//}
